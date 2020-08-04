a8c0f304675dbd9fdab4d37d7db749a5ac99c382
ArieleM <ariele_apm@outlook.com>
2020-08-04 19:59:09 -0300

    Estilizando a Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 
 const HomeScreen = ()=>{
   return(
     <>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
-      <Text>Essa é a tela de Home</Text>
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>Séries Ltd</Text>
+      </View>
     </>
   )
 }
+
+const styles= StyleSheet.create({
+  viewStyle:{
+    flex:1 ,
+    alignItems: "center",
+    justifyContent:"center",
+    backgroundColor:"#990193",
+  },
+  textStyle:{
+    color:"white",
+    fontSize:30
+  }
+})
 export default HomeScreen;
+
+
